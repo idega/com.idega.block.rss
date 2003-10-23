300fac1b60c072f0b8b08008ed93ff9b6fa75c7e
Jonas Kari Blandon <jonas@idega.com>
2003-10-23 13:34:22 +0000

    - removed classes no longer used - use int as source id - improved source selection gui a bit

--- src/java/com/idega/block/rss/business/RSSFetcher.java
+++
@@ -1,8 +0,0 @@
-package com.idega.block.rss.business;
-
-
-public interface RSSFetcher extends com.idega.business.IBOService
-{
- public java.util.Collection getLinksAndHeadlines(java.lang.String p0) throws java.rmi.RemoteException;
- public void saveLinksAndHeadlines(String sourceURL) throws java.rmi.RemoteException;
-}
--- src/java/com/idega/block/rss/business/RSSFetcherHome.java
+++
@@ -1,8 +0,0 @@
-package com.idega.block.rss.business;
-
-
-public interface RSSFetcherHome extends com.idega.business.IBOHome
-{
- public RSSFetcher create() throws javax.ejb.CreateException, java.rmi.RemoteException;
-
-}
