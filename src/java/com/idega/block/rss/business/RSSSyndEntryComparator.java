872519c1af2fdc1fb3de56ba421fa1e9a54482eb
Þórhallur Helgason <laddi@idega.com>
2010-01-27 13:45:05 +0000

    Fixing when entry has no published date.

--- src/java/com/idega/block/rss/business/RSSSyndEntryComparator.java
+++ src/java/com/idega/block/rss/business/RSSSyndEntryComparator.java
@@ -1,20 +1,31 @@
 package com.idega.block.rss.business;
 
 import java.util.Comparator;
+import java.util.Date;
 
 public class RSSSyndEntryComparator implements Comparator<RSSSyndEntry> {
 
 	@Override
 	public int compare(RSSSyndEntry o1, RSSSyndEntry o2) {
-		if (o1.getEntry().getPublishedDate() == null && o2.getEntry().getPublishedDate() != null) {
-			return 1;
+		Date date1 = o1.getEntry().getPublishedDate();
+		if (date1 == null) {
+			date1 = o1.getEntry().getUpdatedDate();
 		}
-		else if (o1.getEntry().getPublishedDate() != null && o2.getEntry().getPublishedDate() == null) {
+		Date date2 = o2.getEntry().getPublishedDate();
+		if (date2 == null) {
+			date2 = o2.getEntry().getUpdatedDate();
+		}
+		
+		if (date1 == null && date2 != null) {
 			return -1;
 		}
-		else if (o1.getEntry().getPublishedDate() == null && o2.getEntry().getPublishedDate() == null) {
+		else if (date1 != null && date2 == null) {
+			return 1;
+		}
+		else if (date1 == null && date1 == null) {
 			return 0;
 		}
-		return (int) (o1.getEntry().getPublishedDate().getTime() - o2.getEntry().getPublishedDate().getTime());
+		
+		return date1.getTime() - date2.getTime() > 0 ? -1 : 1;
 	}
 }
