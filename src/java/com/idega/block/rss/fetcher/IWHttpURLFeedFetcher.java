0938ce39cbf913658397f6310cd68ea3cb759314
Valdas Žemaitis <valdas@idega.com>
2014-08-07 12:19:13 +0300

    Improvements

--- src/java/com/idega/block/rss/fetcher/IWHttpURLFeedFetcher.java
+++ src/java/com/idega/block/rss/fetcher/IWHttpURLFeedFetcher.java
@@ -10,7 +10,9 @@ import org.springframework.beans.factory.annotation.Autowired;
 
 import com.idega.core.file.util.MimeTypeUtil;
 import com.idega.repository.RepositoryService;
+import com.idega.util.CoreConstants;
 import com.idega.util.IOUtil;
+import com.idega.util.StringUtil;
 import com.idega.util.expression.ELUtil;
 import com.sun.syndication.feed.synd.SyndFeed;
 import com.sun.syndication.fetcher.FetcherException;
@@ -23,52 +25,63 @@ import com.sun.syndication.io.XmlReader;
 public class IWHttpURLFeedFetcher extends HttpURLFeedFetcher {
 
 	private static final Logger LOGGER = Logger.getLogger(IWHttpURLFeedFetcher.class.getName());
-	
+
 	@Autowired
 	private RepositoryService repository;
-	
+
 	private RepositoryService getRepositoryService() {
 		if (repository == null) {
 			ELUtil.getInstance().autowire(this);
 		}
 		return repository;
 	}
-	
+
 	public IWHttpURLFeedFetcher(FeedFetcherCache feedInfoCache) {
 		super(feedInfoCache);
 	}
-	
+
+	@Override
 	public SyndFeed retrieveFeed(URL feedUrl) throws IllegalArgumentException, IOException, FeedException, FetcherException {
 		if (feedUrl == null) {
 			LOGGER.warning("URL for RSS feed is not provided");
 			return null;
 		}
-		
+
 		RepositoryService repository = getRepositoryService();
 		if (repository == null) {
 			return super.retrieveFeed(feedUrl);
 		}
-		
+
 		InputStream stream = null;
+		SyndFeed feed = null;
 		try {
 			String pathInRepository = feedUrl.getPath();
-			if (!repository.getExistence(pathInRepository)) {
-				return null;
-			}
-			
-			stream = repository.getInputStreamAsRoot(pathInRepository);
+			if (!StringUtil.isEmpty(pathInRepository) && pathInRepository.startsWith(CoreConstants.WEBDAV_SERVLET_URI) || pathInRepository.startsWith(CoreConstants.PATH_FILES_ROOT)) {
+				if (!repository.getExistence(pathInRepository)) {
+					return null;
+				}
 
-		    XmlReader reader = new XmlReader(stream, MimeTypeUtil.MIME_TYPE_XML, true);  
-		    
-		    SyndFeedInput syndFeedInput = new SyndFeedInput();
-		    syndFeedInput.setPreserveWireFeed(isPreserveWireFeed());
-		    
-			return syndFeedInput.build(reader);
+				stream = repository.getInputStreamAsRoot(pathInRepository);
+
+			    XmlReader reader = new XmlReader(stream, MimeTypeUtil.MIME_TYPE_XML, true);
+
+			    SyndFeedInput syndFeedInput = new SyndFeedInput();
+			    syndFeedInput.setPreserveWireFeed(isPreserveWireFeed());
+
+				feed = syndFeedInput.build(reader);
+			} else {
+				feed = super.retrieveFeed(feedUrl);
+			}
 		} catch (Exception e) {
 			LOGGER.log(Level.WARNING, "Error getting RSS feed from " + feedUrl, e);
-			return super.retrieveFeed(feedUrl);
+			feed = super.retrieveFeed(feedUrl);
 		} finally {
 			IOUtil.close(stream);
 		}
+
+		if (feed == null) {
+			LOGGER.warning("Error getting feed from " + feedUrl);
+		}
+		return feed;
 	}
 }
