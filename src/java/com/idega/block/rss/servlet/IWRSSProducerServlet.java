59addf2994a93564855e8c2abe707419a96c7a16
Valdas Žemaitis <valdas@idega.com>
2015-08-20 14:08:28 +0300

    Printing only warning

--- src/java/com/idega/block/rss/servlet/IWRSSProducerServlet.java
+++ src/java/com/idega/block/rss/servlet/IWRSSProducerServlet.java
@@ -10,6 +10,7 @@
 package com.idega.block.rss.servlet;
 
 import java.io.IOException;
+import java.util.logging.Logger;
 
 import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
@@ -23,35 +24,35 @@ import com.idega.block.rss.data.RSSRequest;
 /**
  * @see com.idega.block.rss.business.RSSProducer Finds the correct RSSProducer
  *      and delegates the task of getting an rss feed to it.
- * 
+ *
  * Last modified: $Date: 2007/04/26 18:59:10 $ by $Author: justinas $
- * 
+ *
  * @author <a href="mailto:eiki@idega.com">eiki</a>
  * @version $Revision: 1.4 $
  */
 public class IWRSSProducerServlet extends HttpServlet {
 
-	/**
-	 * Comment for <code>serialVersionUID</code>
-	 */
 	private static final long serialVersionUID = 8202220239727353313L;
 
+	private static final Logger LOGGER = Logger.getLogger(IWRSSProducerServlet.class.getName());
+
+	@Override
 	public void doGet(HttpServletRequest _req, HttpServletResponse _res) throws IOException {
 		doPost(_req, _res);
 	}
 
+	@Override
 	public void doPost(HttpServletRequest request, HttpServletResponse response) throws IOException {
 
 		RSSProducerRegistry factory = RSSProducerRegistry.getInstance();
 		RSSRequest rssReq = new RSSRequest(request, response);
-//		rssReq.setServletContext(this.getServletContext());
 
 		try {
 			RSSProducer producer = factory.getRSSProducerByIdentifier(rssReq.getIdentifier());
 			producer.handleRSSRequest(rssReq);
 		} catch (NoSuchRSSProducerException e) {
+			LOGGER.warning("RSS producer '" + rssReq.getIdentifier() + "' does not exist!");
 			response.getWriter().print(e.getMessage());
-			e.printStackTrace();
 			response.getWriter().flush();
 			response.getWriter().close();
 		}
